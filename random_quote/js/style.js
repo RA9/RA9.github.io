29d894952ffc5a1bee931d0115db604bca813edf
RA9 <cnah27@gmail.com>
2017-11-08 00:10:22 +0000

    modifying files

--- random_quote/js/style.js
+++ random_quote/js/style.js
@@ -1,21 +1,62 @@
-function randomQuote() {
-  $.ajax({
-      url: "https://api.forismatic.com/api/1.0/?",
-      dataType: "jsonp",
-      data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
-      success: function( response ) {
-        $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
-          response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
-        $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
-          ' (' + response.quoteAuthor + ')');
-      }
-  });
+
+function startTime() {
+    var today=new Date();
+    var h=today.getHours();
+    var m=today.getMinutes();
+    var s=today.getSeconds();
+    var ampm = "";
+    m = checkTime(m);
+
+    if (h > 12) {
+    	h = h - 12;
+    	ampm = " PM";
+    } else if (h == 12){
+        h = 12;
+    	ampm = " AM";
+    } else if (h < 12){
+        ampm = " AM";
+    } else {
+        ampm = "PM";
+    };
+  
+  if(h==0) {
+    h=12;
+  }
+    
+    document.getElementById('display').innerHTML = h+":"+m+ampm;
+    var t = setTimeout(function(){startTime();},500);
 }
 
-$(function() {
-  randomQuote();
+function checkTime(i) {
+    if (i<10) {i = "0" + i;}  // add zero in front of numbers < 10
+    return i;
+}
+
+//Start Date
+function startDate() {
+  var d = new Date();
+  var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+  document.getElementById("date").innerHTML = days[d.getDay()]+" | "+[d.getMonth()+1]+"/"+d.getDate()+"/"+d.getFullYear();
+}
+
+//The forismatic Api
+$(document).ready(function(){
+ getQuote();
+ var quotes;
+ var author;
+ function getQuote(){
+    
+  var url = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=?";
+  $.getJSON(url,function(data){
+    $("#quote").html('"'+data.quoteText+'"');
+    $("#author").html("-" + data.quoteAuthor);
+  });
+  }
+  $('.share.twitter').on("click",function(){
+	  window.open("https://twitter.com/intent/tweet?text=" +quotes);
+  });
+  $('.get-quote').on("click",function(){
+  getQuote();
+  });
 });
 
-$("button").click(function(){
-  randomQuote();
-});
